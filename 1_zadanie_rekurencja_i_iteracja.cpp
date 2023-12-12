b9de67ef9daebed9ae385dd30dbba24f87aba72e
sudosec <68482375+bvsab-dev@users.noreply.github.com>
2023-12-12 19:57:20 +0100

    Update 1_zadanie_rekurencja_i_iteracja.cpp

--- 1_zadanie_rekurencja_i_iteracja.cpp
+++ 1_zadanie_rekurencja_i_iteracja.cpp
@@ -10,19 +10,19 @@ int rekurencja(int n) {
 }
 
 int iteracja(int n) {
-    int a = 2;
-    int b = 0;
-    int c = 1;
+    int a = 2;//wyraz 0
+    int b = 0;//wyraz 1
+    int c = 1;//wyraz 2
     int wynik = 0;
     if(n==0) return 2;
     if(n==1) return 0;
     if(n==2) return 1;
     
-       for (int i = 3; i <= n; ++i) {
-        wynik = c - 2 * b + a;
-        a = b;
-        b = c;
-        c = wynik;
+       for (int i = 3; i <= n; ++i) { //zaczynamy od 3 wyrazu gdyz wyraz 2 jest zdefiniowany
+        wynik = c - 2 * b + a;//uzywamy wzoru c to wyraz n-1 b to wyraz n-2 a to wyraz n-3
+        a = b; //przypisujemy wyraz n-3 jako kolejny wyraz czyli n-2
+        b = c;//analogicznie jak wyzej tylko tutaj wyraz n-2 przypisujemy n-1
+        c = wynik;//przypisz n-1 naszemu wynikowi ktory otrzymalismy wyzej przez obliczenia
     }
     return wynik;
 }
