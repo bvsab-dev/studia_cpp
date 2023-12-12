23ef866a5a6771294fb34fe2260623cf0b8b8df2
sudosec <68482375+bvsab-dev@users.noreply.github.com>
2023-12-12 20:13:18 +0100

    Update 5_zadanie_struktura.cpp

--- 5_zadanie_struktura.cpp
+++ 5_zadanie_struktura.cpp
@@ -6,14 +6,14 @@ struct lokomotywa {
     int vmax;
     char naped;
     float masa;
-};
+};//definicja struktury
 
 int szybka(lokomotywa szb[], int rozmiar) {
-    int index = 0;
+    int index = 0;//poczatkowy index ustaw na zero od tego zaczyna sie tablica
     
-    for(int i = 1; i < rozmiar; i++) {
-        if(szb[i].vmax > szb[index].vmax) {
-            index = i;
+    for(int i = 1; i < rozmiar; i++) {//zacznij od elementu 1
+        if(szb[i].vmax > szb[index].vmax) {//porownaj czy element i jest wiekszy od elementu index czyli obecnie przypisanej najwiekszej wartosci
+            index = i;//jesli tak to przypisz do zmiennej index wartosc i
         }
     }
 
@@ -23,8 +23,8 @@ int szybka(lokomotywa szb[], int rozmiar) {
 
 int main() {
     lokomotywa lokus;
-    lokomotywa tab[5] = {{100,'e',21.98},{215,'s',28.78},{99,'p',100.78},{300,'i',20.99},{98,'p',56.78}};
-    cout<<szybka(tab,5);
+    lokomotywa tab[5] = {{100,'e',21.98},{215,'s',28.78},{99,'p',100.78},{300,'i',20.99},{98,'p',56.78}};//definicja tablicy stuktur
+    cout<<szybka(tab,5);//podaj tablice na ktorej ma operowac funkcja oraz jej rozmiar
      
     return 0;
 }
